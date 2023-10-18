7007b6f7b97973452f8d2015f8857c06937db447
Ikshit Dhyani <90301781+ikshitd@users.noreply.github.com>
2023-10-18 19:59:39 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,59 +1,57 @@
 #include <stdio.h> 
-#include <unistd.h> 
-#include <math.h> 
-
-typedef struct fcfs {
-	int process; 
-	int burst; 
-	int arrival;
+typedef struct Process {
+	int id; 
+	int ct; 
+	int arrival; 
+	int burst;
 	int tat; 
-	int wt; 
-	int ct;  
-}fcfs; 
-
-void sort(fcfs *a, int n) 	{
-	for(int i = 0; i < n - 1; i++) { 
-		for(int j = 0; j < n - i - 1; j++) {
-			if(a[j].arrival > a[j + 1].arrival) {
-				fcfs k = a[j]; 
-				a[j] = a[j + 1]; 
-				a[j + 1] = k; 
-			}
-		}
-	}
-	return; 
-}
+	int wt;
+}P;
 int main() {
 	int n; 
 	printf("Enter the number of processes"); 
 	puts(""); 
 	scanf("%d", &n); 
-	fcfs a[n]; 
+	P a[n]; 
+	int temp[n]; 
 	for(int i = 0; i < n; i++) {
-		a[i].process = i + 1;
-		printf("Enter the cpu burst time "); 
-		scanf("%d", &(a[i].burst)); 
-		printf("Enter the arrival time "); 
-		scanf("%d", &(a[i].arrival)); 
+		printf("Enter the arrival and the burst time of the %d process: ", i + 1); 
+		a[i].id = i + 1; 
+		scanf("%d%d", &(a[i].arrival), &(a[i].burst)); 
+		temp[i] = a[i].burst; 
+		puts(""); 
 	}
-	sort(a, n); 
-	for(int i =0 ; i < n; i++) {
-		printf("%d %d %d", a[i].process, a[i].burst, a[i].arrival); 
-		puts("");  
+	int done = 0;
+	int at = -1; 
+	while(done != n) {
+		int p = -1;  // stores the index of the process to be executed at
+		int tempat = 1e9 + 7;
+		for(int i =0 ; i < n; i++) {
+			if(a[i].arrival <= at && temp[i]) {
+				if(a[i].arrival < tempat) {
+					p = i; 
+					tempat = a[i].arrival; 
+				}
+			}
+		}
+		if(p == -1) at++; 
+		else {
+			at += temp[p]; 
+			temp[p] = 0; 
+			a[p].ct = at; 
+			done++; 
+		}
 	}
-	printf("Process\tArrival time\t Burst time\t Completiton Time\tturn around time\t waiting time");
+printf("Process\tArrival time\t Burst time\t Completiton Time\tturn around time\t waiting time");
 	puts(""); 
-	int current_time = 0; 
 	double average_tat = 0; 
 	double average_wt = 0;
 	for(int i = 0; i < n; i++) {
-		current_time += a[i].burst; 
-		a[i].ct = current_time; 
 		a[i].tat = a[i].ct - a[i].arrival; 
 		a[i].wt = a[i].tat - a[i].burst; 
 		average_tat += a[i].tat; 
 		average_wt += a[i].wt; 
-		printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t%d", a[i].process, a[i].arrival, a[i].burst, a[i].ct, a[i].tat, a[i].wt); 
+		printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d", a[i].id, a[i].arrival, a[i].burst, a[i].ct, a[i].tat, a[i].wt); 
 		puts(""); 
 	}
 	printf("The average waiting time is : %lf and the average turn around time is : %lf", average_wt / (double)n, average_tat / (double)n); 
